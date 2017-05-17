9291dd61a92f8450ba41975da8911b2a54b46a2c
devliu <devliu.official@foxmail.com>
2017-05-17 18:36:09 +0800

    dependencies init

--- app/src/main/java/com/devliu/learning/MainActivity.java
+++ app/src/main/java/com/devliu/learning/MainActivity.java
@@ -1,12 +1,40 @@
 package com.devliu.learning;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import javax.inject.Inject;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv_id)
+    TextView tvId;
+    @Inject
+    Shoe shoe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_act);
+        ButterKnife.bind(this);
+
+        MainComponent build = DaggerMainComponent.builder()
+                .build();
+
+        build.inject(this);
+
+        tvId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, shoe.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
 }
---
+++ app/src/main/java/com/devliu/learning/Shoe.java
@@ -0,0 +1,22 @@
+package com.devliu.learning;
+
+import javax.inject.Inject;
+
+/**
+ * Created by liuhao
+ * on 2017/5/9
+ * use to :
+ */
+
+public class Shoe {
+
+    @Inject
+    public Shoe() {
+    }
+
+    @Override
+    public String toString() {
+        return "鞋子";
+    }
+
+}
